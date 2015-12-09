7a780e159ccb395f974c528e1a5bbd6deb22b00a
Elias Ruemmler <git@rc-art.de>
2015-12-10 00:00:49 +0100

    Kommentare ergänzt

--- TimetableGenForms/ClassEnum.cs
+++ TimetableGenForms/ClassEnum.cs
@@ -9,10 +9,19 @@ namespace Rca.TtGen
 {
     public enum ClassEnum
     {
+        /// <summary>
+        /// OR8 - Verbrenner Offroad 1:8 Buggy
+        /// </summary>
         [Description("OR8")]
         Or8 = 0,
+        /// <summary>
+        /// ORT - Verbrenner Offroad 1:8 Truggy
+        /// </summary>
         [Description("ORT")]
         Ort,
+        /// <summary>
+        /// ORE8 - Elektro Offroad 1:8 Buggy
+        /// </summary>
         [Description("ORE8")]
         Ore8
     }
